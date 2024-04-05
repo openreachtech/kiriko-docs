3966cfe7f163c896458a36b0a8a5f60b55663447
hecticme <hangoc.minh1306@gmail.com>
2024-04-05 12:02:55 +0700

    add basic `@stylisic/eslint-plugin` rules

--- eslint.config.js
+++ eslint.config.js
@@ -13,6 +13,64 @@ export default [
     plugins: {
       '@stylistic': pluginStylistic,
     },
+    rules:{
+      '@stylistic/comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'never',
+        },
+      ],
+      '@stylistic/indent': [
+        'error',
+        2,
+        {
+          ignoredNodes: [],
+          SwitchCase: 1, // 0
+          VariableDeclarator: 1,
+          outerIIFEBody: 1,
+          MemberExpression: 1,
+          FunctionDeclaration: {
+            parameters: 1,
+            body: 1,
+          },
+          FunctionExpression: {
+            parameters: 1,
+            body: 1,
+          },
+          StaticBlock: {
+            body: 1
+          },
+          CallExpression: {
+            arguments: 1.
+          },
+          ArrayExpression: 1,
+          ObjectExpression: 1,
+          ImportDeclaration: 1,
+          flatTernaryExpressions: false,
+          offsetTernaryExpressions: false,
+          ignoreComments: false,
+        },
+      ],
+      '@stylistic/quotes': [
+        'error',
+        'single', // 'double'
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+      '@stylistic/semi': [
+        'error',
+        'never', // 'always'
+        {
+          beforeStatementContinuationChars: 'never',
+        },
+      ],
+    },
   },
 
   // For `.vue` files
