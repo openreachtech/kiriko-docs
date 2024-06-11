f2c86b05d8afa73bfbe228420932b3008d6cbc3d
EG-easy <1018eguchi@gmail.com>
2024-06-11 15:39:52 +0800

    update vitepress config

---
+++ .vitepress/config/shared.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitepress'
+
+
+export const shared = defineConfig({
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/openreachtech/kiriko-docs',
+      },
+    ],
+    footer: {
+      message: 'Built by the <a href="https://cryptolearningclub.org">Crypto Learning Club</a> Community.',
+      copyright: '© 2024 kiriko.wtf. ',
+    },
+  },
+})
